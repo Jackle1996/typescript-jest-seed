9be529248c35c4b9943cf02a6f5e90c243201730
Yannick Luthiger <yannick.luthiger@stud.hslu.ch>
2019-09-03 15:30:43 +0200

    if if if

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,27 @@
+import { stringLiteral } from "@babel/types";
+
 class Charles {
+  hey(message: string) {
+    let trim = message.trim();
+    let isEmpty: boolean = trim == "";
+    let isCapsLock: boolean = trim == trim.toUpperCase() && !!trim.match(/[A-Z]/);
+    let isQuestion: boolean = trim.slice(-1) == '?';
+
+    if(isEmpty) {
+      return "Fine. Be that way!";
+    }
+    if(isCapsLock && isQuestion) {
+      return "Calm down, I know what I\'m doing!"
+    }
+    if(isCapsLock) {
+      return "Whoa, chill out!";
+    }
+    if(isQuestion) {
+      return "Sure.";
+    }
+
+    return "Whatever.";
+  }
   
   // Your code here
 
