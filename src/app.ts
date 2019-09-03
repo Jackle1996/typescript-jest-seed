6422184dd0ef66a577fa5539aa8905bf7c14d1a5
Yannick Luthiger <yannick.luthiger@stud.hslu.ch>
2019-09-03 13:56:52 +0200

    one line to rule them all

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-class TwoFer {
-  
-  // Your code here
+import { stringLiteral } from "@babel/types";
 
+class TwoFer {
+  static twoFer(name: string = "you"): string {
+    return `One for ${name}, one for me.`;
+  }
 }
 
 export default TwoFer
